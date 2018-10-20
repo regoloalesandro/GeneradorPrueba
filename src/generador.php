58e1bd2ebb8486c8b999ab80638a4640e20c7156
Alesandro Regolo <regoloalesandro@gmail.com>
2018-10-20 17:56:13 -0300

    cambios en el constructor de generador.php

--- src/generador.php
+++ src/generador.php
@@ -1,42 +1,20 @@
 <?php
 		namespace MultipleChoice;
+		require_once __DIR__ . '/../vendor/autoload.php';
 		use Symfony\Component\Yaml\Yaml;
 	class generador{
-			protected $cantPreguntas;
-			protected $preguntas;
+		protected $cantPreguntas;
+		protected $preguntas;
 			public function __construct($cantPreguntas = 12){
-			    $this->preguntas = Yaml::parseFile('Preguntas/preguntas.yml');
-		    	if($cantPreguntas < count($this->preguntas['preguntas']) && $cantPreguntas > 0){
-		       		$this->cantPreguntas = $cantPreguntas;
-		    	}else{
-		        	$this->cantPreguntas = 12;            
-		    }
-		}
-			public function organizar(){
-		   		$this->preguntas = $this->mezclar($this->preguntas['preguntas'],$this->cantPreguntas);
-		}
-			public function mezclar($array, $cant){
-		    	shuffle($array);
-		    	for($i = count($array);$i>$cant;$i--){
-		        array_pop($array);
-		   		}
-		    return $array;
-		}
-			public function devolverPreguntas(){
-		   		return $this->preguntas['preguntas'];
-		}
-			public function devolverEnunciado($pregunta){
-		   		return $pregunta['descripcion'];
-		}
-			public function devolverCantidad(){
-		    	return $this->cantPreguntas;
-		}
-			public function devolverRespuestas($pregunta){
-		    	$aux = $pregunta['respuestas_incorrectas'];
-		    	$cant = count($pregunta['respuestas_correctas']);
-		    	for($i = 0; $i<$cant;$i++){
-		        	array_push($aux,$pregunta['respuestas_correctas'][$i]);
+			public function __construct($file){
+				$this->preguntas = Yaml::parseFile($file);	
 		    	}
-		   		return $aux;
-		}
+		   	public function getCantPreguntas(){
+			return count($this->preguntas);
+		    	}
+			public function Tomarpreguntas(){
+			
+			}
+			
+		
 }
---
+++ test/Multiplechoicetest.php
@@ -0,0 +1,12 @@
+<?php
+namespace GeneradorPrueba;
+use PHPUnit\Framework\TestCase;
+class GeneradorTest extends TestCase {
+    public function testGenerar() {
+        $generador = new Generador('./tests/preguntas.yml');
+        
+        $this->assertTrue(isset($generador));
+        $this->assertEquals($generador->getCantPreguntas(), 25);
+   
+    }
+}
