694548de9701cc6ef60c2f7dab5ac86a1794cd0c
Alesandro Regolo <regoloalesandro@gmail.com>
2018-10-20 19:05:38 -0300

    eliminacion del archivo multiplechoice.php

--- src/generador.php
+++ src/generador.php
@@ -1,26 +1,30 @@
 <?php
-		namespace GeneradorPrueba;
-		require_once __DIR__ . '/../vendor/autoload.php';
-		use Symfony\Component\Yaml\Yaml;
+
+namespace GeneradorPrueba;
+require_once __DIR__ . '/../vendor/autoload.php';
+use Symfony\Component\Yaml\Yaml;
 	class generador{
-	 protected $preguntasYaml;
-  	 protected $yaml;
-     protected $lecturaYaml;
-     protected $preguntas = [];
-     public function __construct($archivo){
-   		$this->yaml = new Parser();
-    	$this->value = $this->yaml->parse(file_get_contents($archivo));
-    	$this->preguntasYaml = $this->value['preguntas'];
+	 	protected $preguntasYaml;
+  	 	protected $yaml;
+     	protected $lecturaYaml;
+     	protected $preguntas = [];
+    	 	public function __construct($archivo){
+   			$this->yaml = new Parser();
+    		$this->value = $this->yaml->parse(file_get_contents($archivo));
+    		$this->preguntasYaml = $this->value['preguntas'];
   		}
-  	public function crearPreguntas(){
-    	$cantPreguntas = count($this->preguntasYaml);
-   		for ($i=0; $i < $cantPreguntas; $i++) {
-      		$this->preguntas[$i] = new Pregunta ($this->preguntasYaml[$i]);
+  		public function crearPreguntas(){
+    		$cantPreguntas = count($this->preguntasYaml);
+   			for ($i=0; $i < $cantPreguntas; $i++) {
+      			$this->preguntas[$i] = new Pregunta ($this->preguntasYaml[$i]);
     	}
     	echo var_dump( $this->preguntas);
-  	}
-
 	
-	public function mezclar(){
-       		shuffle($this->preguntas);
+		}
+		public function mezclar(){
+			shuffle($this->preguntas);
+ 			}  
+
+
 	}
+
