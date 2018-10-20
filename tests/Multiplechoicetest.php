c525cbd4cef38dff0471ea7d21452abbe6df64ef
Alesandro Regolo <regoloalesandro@gmail.com>
2018-10-20 18:43:32 -0300

    correccion al llamar a la clase (empieza con minuscula)

---
+++ tests/Multiplechoicetest.php
@@ -0,0 +1,14 @@
+<?php
+namespace GeneradorPrueba;
+
+use PHPUnit\Framework\TestCase;
+
+class GeneradorTest extends TestCase {
+    public function testGenerar() {
+        $generador = new generador('./tests/preguntas.yml');
+        
+        $this->assertTrue(isset($generador));
+        $this->assertEquals($generador->getCantPreguntas(), 25);
+   
+    }
+}
