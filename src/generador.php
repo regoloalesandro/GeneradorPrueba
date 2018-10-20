af4f1774bb36595b9a4b8b31f63e6bc6efb49756
Alesandro Regolo <regoloalesandro@gmail.com>
2018-10-20 18:22:55 -0300

    Creacion de tomarpreguntas en generador.php

--- src/generador.php
+++ src/generador.php
@@ -9,12 +9,20 @@
 			public function __construct($file){
 				$this->preguntas = Yaml::parseFile($file);	
 		    	}
+
 		   	public function getCantPreguntas(){
 			return count($this->preguntas);
 		    	}
+
 			public function Tomarpreguntas(){
-			
-			}
-			
-		
+        		$this->preguntas = $this->mezclar($this->preguntas['preguntas'],$this->getCantPreguntas());
+    			}
+
+    		public function mezclar($array, $cant){
+        		shuffle($array);
+        		for($i = count($array);$i>$cant;$i--){
+            		array_pop($array);
+       			}
+        		return $array;
+   			 }
 }
