e1dbfe79416e57f62f2d60bdce7b83f3f9953945
antelor <ramiro.antelo4@gmail.com>
2018-10-21 20:40:14 -0300

    cambios a construct generador

--- src/generador.php
+++ src/generador.php
@@ -3,28 +3,23 @@
 namespace GeneradorPrueba;
 require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\Yaml\Yaml;
-	class generador{
-	 	protected $preguntasYaml;
-  	 	protected $yaml;
-     	protected $lecturaYaml;
-     	protected $preguntas = [];
-    	 	public function __construct($archivo){
-   			$this->yaml = new Parser();
-    		$this->value = $this->yaml->parse(file_get_contents($archivo));
-    		$this->preguntasYaml = $this->value['preguntas'];
-  		}
-  		public function crearPreguntas(){
-    		$cantPreguntas = count($this->preguntasYaml);
-   			for ($i=0; $i < $cantPreguntas; $i++) {
-      			$this->preguntas[$i] = new Pregunta ($this->preguntasYaml[$i]);
-    		}
-    	echo var_dump( $this->preguntas);
-	
-		}
-		public function mezclar(){
-			shuffle($this->preguntas);
- 			}  
 
+class Generador{
+	protected $preguntas = [];
 
+	public function __construct($archivo){
+		$yaml = new Parser();
+		$value = $yaml->parse(file_get_contents($archivo));
+		$preguntasYaml = $value['preguntas'];
+
+		for ($i=0; $i < count($preguntasYaml); $i++) {
+			$this->preguntas[$i] = new Pregunta ($preguntasYaml[$i]);
+		}	
 	}
+
+	public function mezclar(){
+		shuffle($this->preguntas);
+	}
+
+}
 
