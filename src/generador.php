bf5517dc26d15a61d90b20a81c8af91696ed113f
antelor <ramiro.antelo4@gmail.com>
2018-10-21 21:18:28 -0300

    agregue atributo preguntas

--- src/generador.php
+++ src/generador.php
@@ -5,11 +5,17 @@ namespace GeneradorPrueba;
 use Symfony\Component\Yaml\Yaml;
 
 class Generador {
-    protected $preguntas;
+    protected $archivo;
+	protected $preguntas;
     
 	public function __construct($file){
-        $this->preguntas = Yaml::parseFile($file);	
+        $this->archivo = Yaml::parseFile($file);
+		$this->preguntas = $archivo['preguntas'];
     }
+
+	public function getPreguntas(){
+		return $this->preguntas;
+	}
 
     public function getCantPreguntas(){
         return count($this->preguntas);
