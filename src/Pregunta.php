a809081162e200ea385e3e809f8f33c66056aa7d
antelor <ramiro.antelo4@gmail.com>
2018-10-27 19:08:50 -0300

    creacion pregunta

---
+++ src/Pregunta.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace GeneradorPrueba;
+
+use Symfony\Component\Yaml\Yaml;
+
+class Pregunta {
+    protected $descripcion;
+    protected $respuestas_correctas;
+    protected $respuestas_incorrectas;
+    protected $ocultar_opcion_todas_las_anteriores=false;
+    protected $ocultas_opcion_ninguna_de_las_anteriores=false;
+    protected $texto_ninguna_de_las_anteriores="";
+
+    
+	public function __construct($info){
+        $this->descripcion=$info["descripcion"];
+        $this->$respuestas_correctas = $info["respuestas_correctas"];
+        $this->$respuestas_incorrectas = $info["respuestas_incorrectas"];
+
+        if( isset($info["ocultar_opcion_todas_las_anteriores"]) ){
+            $this->$ocultar_opcion_todas_las_anteriores = $info["ocultar_opcion_todas_las_anteriores"];
+        }
+
+        if( isset($info["ocultas_opcion_ninguna_de_las_anteriores"]) ){
+            $this->$ocultas_opcion_ninguna_de_las_anteriores = $info["ocultas_opcion_ninguna_de_las_anteriores"];
+        }
+
+        if( isset($info["texto_ninguna_de_las_anteriores"])){
+            $this->$texto_ninguna_de_las_anteriores = $info["texto_ninguna_de_las_anteriores"];
+        }
+
+    }
+
+    public function getRespCorrectas(){
+        return $this->respuestas_correctas;
+    }
+
+    public function getRespIncorrectas(){
+        return $this->respuestas_incorrectas;
+    }
+}
---
+++ tests/PreguntaTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GeneradorPrueba;
+
+use PHPUnit\Framework\TestCase;
+
+class Pregunta extends TestCase {
+    public function testGenerarPreg() {
+        $preg_text = Yaml::parse("descripcion: El término pixel hace referencia a
+        respuestas_correctas:
+          - La unidad mínima de información de una imagen.
+        respuestas_incorrectas:
+          - La longitud de la diagonal de una imagen en pulgadas.
+          - La cantidad de puntos por pulgada.
+          - La cantidad de colores de un punto.
+        ocultar_opcion_todas_las_anteriores: true");
+
+        $pregunta = new Pregunta($preg_text);
+        
+        $this->assertTrue(isset($pregunta));   
+    }
+
+    public function TestCantPreguntas() {
+        $generador = new Generador('./tests/preguntas.yml');
+        $this->assertEquals(25, $generador->getCantPreguntas());   
+
+    }
+}
