59c8a777c858ced9313000049e8c7ae0f24179e0
Ramiro Antelo <ramiro.antelo4@gmail.com>
2018-11-07 14:32:50 -0300

    error en texto ndla

--- src/Pregunta.php
+++ src/Pregunta.php
@@ -10,7 +10,7 @@ class Pregunta {
     protected $respuestas_incorrectas;
     protected $ocultar_opcion_todas_las_anteriores=false;
     protected $ocultas_opcion_ninguna_de_las_anteriores=false;
-    protected $texto_ninguna_de_las_anteriores="";
+    protected $texto_ninguna_de_las_anteriores = "Ninguna de las anteriores";
 
     
 	public function __construct($info){
